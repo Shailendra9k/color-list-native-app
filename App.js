55d7a2980ba4d6602a19d111719e39d13a69ccd3
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-09-16 17:21:34 +0300

    app updated

--- App.js
+++ App.js
@@ -7,18 +7,22 @@ export default class ColorList extends React.Component {
     this.state = {
       backgroundColor: "#eb4d4b"
     };
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  changeColor(backgroundColor) {
+    this.setState({ backgroundColor });
   }
   render() {
     const { backgroundColor } = this.state;
     return (
-      <View
-        style={[
-          styles.container,
-          {
-            backgroundColor
-          }
-        ]}
-      ></View>
+      <View style={[styles.container, { backgroundColor }]}>
+        <View style={styles.row}>
+          <View style={styles.sample}>
+            <Text>Yellow</Text>
+          </View>
+        </View>
+      </View>
     );
   }
 }
@@ -28,5 +32,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flex: 1,
     alignItems: "center"
+  },
+  button: {
+    fontSize: 30,
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    alignSelf: "stretch",
+    textAlign: "center"
   }
 });
