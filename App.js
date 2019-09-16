b00403c0bf4432845ccf22fb741a9a364cb2b20f
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-09-16 17:06:33 +0300

    app updated

--- App.js
+++ App.js
@@ -1,39 +1,32 @@
 import React from "react";
 import { View, Text, StatusBar, StyleSheet } from "react-native";
 
-export default class App extends React.Component {
+export default class ColorList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      backgroundColor: "#eb4d4b"
+    };
+  }
   render() {
+    const { backgroundColor } = this.state;
     return (
-      <View style={styles.container}>
-        <StatusBar hidden={true} />
-        <Text style={styles.defaultText}>React</Text>
-        <Text style={[styles.defaultText, styles.selectedText]}>Node</Text>
-        <Text style={styles.defaultText}>JS</Text>
-      </View>
+      <View
+        style={[
+          styles.container,
+          {
+            backgroundColor
+          }
+        ]}
+      ></View>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#0984e3",
-    flexDirection: "row",
     justifyContent: "center",
     flex: 1,
     alignItems: "center"
-  },
-  defaultText: {
-    flex: 1,
-    textAlign: "center",
-    fontSize: 22,
-    padding: 10,
-    margin: 5,
-    color: "#2d3436",
-    borderWidth: StyleSheet.hairlineWidth
-  },
-  selectedText: {
-    backgroundColor: "#ffeaa7",
-    color: "#d63031",
-    fontWeight: "bold",
-    alignSelf: "flex-end"
   }
 });
